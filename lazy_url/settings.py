4cb5ad6bbddf2059089032becd85d23586b000ec
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2022-06-08 21:55:50 +0300

    added app users

--- lazy_url/settings.py
+++ lazy_url/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.LazyUser'
