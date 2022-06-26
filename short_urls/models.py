7984c4722386f78c3d5c3d7fba4e086dbf5909f6
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2022-06-26 22:18:41 +0300

    added clicks_per_day field to Url model for future functionality

--- short_urls/models.py
+++ short_urls/models.py
@@ -5,6 +5,7 @@ class Url(models.Model):
     long_url = models.URLField(max_length=2048)
     short_url = models.URLField()
     password = models.IntegerField()
+    clicks_per_day = models.IntegerField(default=0)  # not implemented yet
     click = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
 
