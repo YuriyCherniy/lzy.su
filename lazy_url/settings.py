791f305a5d7edb9af93891bb752cd105ae378429
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2022-07-08 12:38:49 +0300

    minor changes

--- lazy_url/settings.py
+++ lazy_url/settings.py
@@ -141,7 +141,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
