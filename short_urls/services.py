93e369afa0101768ad6eab7e2fbc8ee1cfd78c3c
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2022-07-09 14:51:46 +0300

    fixed create_url_object function for genirating first entry in db

--- short_urls/services.py
+++ short_urls/services.py
@@ -10,7 +10,13 @@ hashids = Hashids()
 def create_url_object(long_url):
     """Genirate a short url identifier and create Url object"""
 
-    short_url = hashids.encode(Url.objects.last().pk + 1)
+    # genirate pk for first object in new db
+    try:
+        pk_for_hash = Url.objects.last().pk + 1
+    except Url.DoesNotExist:
+        pk_for_hash = 1
+
+    short_url = hashids.encode(pk_for_hash)
     url_obj = Url.objects.create(
         long_url=long_url,
         short_url=short_url,
