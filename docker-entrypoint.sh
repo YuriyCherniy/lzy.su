985fb10ec2c7a7dbe15587b2d5c5307d80c2dffc
Yuriy Cherniy <amidobox@gmail.com>
2022-07-17 00:43:03 +0300

    added makemigrations command to docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
+python3 manage.py makemigrations
 python3 manage.py migrate
 
 # Start server
