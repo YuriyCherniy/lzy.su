0552e08639f0acaa1ca0b903d60fdd8e30400c47
Yuriy Cherniy <amidobox@gmail.com>
2022-07-19 21:44:34 +0300

    fixed regexp for forbidden domain

--- short_urls/validators.py
+++ short_urls/validators.py
@@ -9,6 +9,7 @@ class LzyURLValidator(URLValidator):
     def __call__(self, value):
         super().__call__(value)
 
-        forbidden_domain = re.search(r'http[s]{0,1}://lzy.su', value)
+        regexp = r'http[s]{0,1}://[w]{0,1}[w]{0,1}[w]{0,1}[\.]{0,1}lzy\.su'
+        forbidden_domain = re.search(regexp, value)
         if forbidden_domain:
             raise ValidationError("This link can't be shortened.")
