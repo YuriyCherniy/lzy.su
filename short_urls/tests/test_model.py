a8fc6e22c92d3b49eceda49a9144406f5cdefb76
Yuriy Cherniy <amidobox@gmail.com>
2022-08-09 23:38:52 +0300

    added tests

--- short_urls/tests/test_model.py
+++ short_urls/tests/test_model.py
@@ -1,6 +1,7 @@
+from tracemalloc import DomainFilter
 from django.test import TestCase
 
-from short_urls.models import Url
+from short_urls.models import Url, ForbiddenDomain
 
 
 class UrlModelTestCase(TestCase):
@@ -13,8 +14,14 @@ class UrlModelTestCase(TestCase):
             user_ip='0.0.0.0'
         )
 
+        ForbiddenDomain.objects.create(domain='lzy.su')
+
     def test_url_model_str_method(self):
         url_obj = Url.objects.get(short_url_hash='test')
         self.assertEqual(
             url_obj.__str__(), '[ http://test.ru/some-long-url-from-the-in ] from ip [0.0.0.0]'
         )
+
+    def test_forbidden_domain_str_method(self):
+        obj = ForbiddenDomain.objects.get(domain='lzy.su')
+        self.assertEqual(obj.__str__(), f'[ {obj.domain} ] created: {obj.created.date()}')
