087c1bc540a11f821c3e176154be08a58c225cd0
Yuriy Cherniy <amidobox@gmail.com>
2022-08-09 23:43:05 +0300

    unregister Group model

--- short_urls/admin.py
+++ short_urls/admin.py
@@ -1,6 +1,10 @@
+from email.headerregistry import Group
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from short_urls.models import Url, ForbiddenDomain
 
 admin.site.register(Url)
 admin.site.register(ForbiddenDomain)
+
+admin.site.unregister(Group)
