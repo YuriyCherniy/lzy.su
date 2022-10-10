51fed6e8b863c171e6d7ed3ecef4325316035dd8
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2022-10-10 10:54:43 +0300

    added tests

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from core.views import IndexView
+from core.views import IndexView, TermsOfUseView
 
 
 class IndexPathTestCase(SimpleTestCase):
@@ -10,3 +10,10 @@ class IndexPathTestCase(SimpleTestCase):
         url = reverse('index-view')
         view = resolve(url).func.view_class
         self.assertEqual(view, IndexView)
+
+
+class TermsOfUsePathTestCase(SimpleTestCase):
+    def test_terms_of_use_is_resolved(self):
+        url = reverse('terms-of-use')
+        view = resolve(url).func.view_class
+        self.assertEqual(view, TermsOfUseView)
--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import reverse
-from django.test import Client, TestCase
+from django.test import Client, TestCase, SimpleTestCase
 
 
 class IndexViewTestCase(TestCase):
@@ -19,3 +19,17 @@ class IndexViewTestCase(TestCase):
         response = self.c.get(reverse('index-view'))
         text = response.context_data.get('text')
         self.assertTrue(text)
+
+
+class TermsOfUseTestCase(SimpleTestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_terms_of_use_view_status_code_200(self):
+        response = self.c.get(reverse('terms-of-use'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_template_used(self):
+        response = self.c.get(reverse('terms-of-use'))
+        self.assertTemplateUsed(response, 'core/terms_of_use.html')
