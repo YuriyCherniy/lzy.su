38bd3f7a87c55e79102e7dff8ad0f14cfc18452e
Yuriy <YuriyCherniyMail@yandex.ru>
2025-04-18 13:41:07 +0300

    .

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd $HOME/lzy.su/certbot
 docker compose run --rm certbot renew --webroot -w /etc/letsencrypt --quiet && \
+cd $HOME/lzy.su && \
 docker compose exec nginx nginx -s reload
