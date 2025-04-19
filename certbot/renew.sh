f07ac73c9d7b48d44dd538d9e17cecd260e61de9
Yuriy <YuriyCherniyMail@yandex.ru>
2025-04-19 23:31:11 +0300

    .

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
-LOGFILE="${HOME}/lzy.su/certbot/renew.log"
-echo "[ $(date) ] === Начало обновления сертификата ===" | tee -a "$LOGFILE"
 
-# Переход в директорию certbot
-cd ${HOME}/lzy.su/certbot
-if [ $? -eq 0 ]; then
-    echo "[ $(date) ] Перешёл в директорию certbot." | tee -a "$LOGFILE"
-else
-    echo "[ $(date) ] Ошибка перехода в директорию certbot!" | tee -a "$LOGFILE"
+log() {
+    echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] $1"
+}
+log_err() {
+    echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] ERROR: $1" >&2
+}
+
+log "=== Certificate renewal process started ==="
+
+log "Switching to certbot directory..."
+cd "${HOME}/lzy.su/certbot"
+if [ $? -ne 0 ]; then
+    log_err "Failed to switch to certbot directory!"
     exit 1
 fi
 
-# Запуск обновления сертификата
-echo "[ $(date) ] Запуск certbot renew..." | tee -a "$LOGFILE"
-docker compose run --rm certbot renew --webroot -w /etc/letsencrypt --quiet 2>&1 | tee -a "$LOGFILE"
-if [ $? -eq 0 ]; then
-    echo "[ $(date) ] Certbot renew завершён успешно." | tee -a "$LOGFILE"
-else
-    echo "[ $(date) ] Ошибка при обновлении сертификата!" | tee -a "$LOGFILE"
+log "Running certbot renew..."
+docker compose run --rm certbot renew --webroot -w /etc/letsencrypt --quiet
+if [ $? -ne 0 ]; then
+    log_err "Certificate renewal failed!"
     exit 2
+else
+    log "Certificate renewal completed successfully."
 fi
 
-# Переход в директорию lzy.su
-cd ${HOME}/lzy.su
-if [ $? -eq 0 ]; then
-    echo "[ $(date) ] Перешёл в директорию lzy.su." | tee -a "$LOGFILE"
-else
-    echo "[ $(date) ] Ошибка перехода в директорию lzy.su!" | tee -a "$LOGFILE"
+log "Switching to lzy.su directory..."
+cd "${HOME}/lzy.su"
+if [ $? -ne 0 ]; then
+    log_err "Failed to switch to lzy.su directory!"
     exit 3
 fi
 
-# Перезапуск nginx
-echo "[ $(date) ] Перезапуск nginx..." | tee -a "$LOGFILE"
-docker compose exec nginx nginx -s reload 2>&1 | tee -a "$LOGFILE"
-if [ $? -eq 0 ]; then
-    echo "[ $(date) ] Перезапуск nginx завершён." | tee -a "$LOGFILE"
-else
-    echo "[ $(date) ] Ошибка перезапуска nginx!" | tee -a "$LOGFILE"
+log "Reloading nginx..."
+docker compose exec nginx nginx -s reload
+if [ $? -ne 0 ]; then
+    log_err "nginx reload failed!"
     exit 4
+else
+    log "nginx reload completed successfully."
 fi
 
-echo "[ $(date) ] === Конец обновления сертификата ===\n" | tee -a "$LOGFILE"
+log "=== Certificate renewal process finished ==="
