9a1b509773499df7b1c92fb97e90ce4f072ff122
Yuriy <YuriyCherniyMail@yandex.ru>
2025-04-19 23:09:42 +0300

    .

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LOGFILE="${HOME}/lzy.su/certbot/renew.log"
+echo "[ $(date) ] === Начало обновления сертификата ===" | tee -a "$LOGFILE"
 cd ${HOME}/lzy.su/certbot
 docker compose run --rm certbot renew --webroot -w /etc/letsencrypt --quiet && \
 cd ${HOME}/lzy.su && \
