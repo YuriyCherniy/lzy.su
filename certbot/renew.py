06c7a323b1b0699f4b6fc9869839532acbc17432
Yuriy <YuriyCherniyMail@yandex.ru>
2025-04-20 21:03:10 +0300

    .

---
+++ certbot/renew.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import subprocess
+from datetime import datetime
+
+def now():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def log(msg):
+    print(f"[ {now()} ] {msg}")
+
+def log_err(msg):
+    print(f"[ {now()} ] ERROR: {msg}", file=sys.stderr)
+
+def main():
+    log("=== Certificate renewal process started ===")
+
+    certbot_dir = os.path.expanduser("~/lzy.su/certbot")
+    try:
+        os.chdir(certbot_dir)
+    except OSError as e:
+        log_err(f"Failed to switch to certbot directory! {e}")
+        sys.exit(1)
+
+    log("Running certbot renew (nginx will reload only if certificate is renewed)...")
+
+    deploy_hook = (
+        f"echo '[ {now()} ] Deploy hook: reloading nginx...' && "
+        f"cd {os.path.expanduser('~')}/lzy.su && "
+        "docker compose exec nginx nginx -s reload && "
+        f"echo '[ {now()} ] Deploy hook: nginx reload completed.'"
+    )
+
+    cmd = [
+        "docker", "compose", "run", "--rm", "certbot", "renew",
+        "--webroot", "-w", "/etc/letsencrypt", "--quiet",
+        "--deploy-hook", deploy_hook
+    ]
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+    if proc.returncode != 0:
+        log_err("Certificate renewal failed! Certbot output:")
+        print(proc.stdout, file=sys.stderr)
+        sys.exit(2)
+    else:
+        log("Certificate renewal process finished. Nginx was reloaded.")
+
+    log("=== Certificate renewal process finished ===")
+
+if __name__ == "__main__":
+    main()
