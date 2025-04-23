60d18d7eb743d331389c610b76d6c686efc901d5
Yuriy <YuriyCherniyMail@yandex.ru>
2025-04-23 15:15:43 +0300

    .

--- app/lazy_url/settings.py
+++ app/lazy_url/settings.py
@@ -98,7 +98,7 @@ POSTGRES_DB = {
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
