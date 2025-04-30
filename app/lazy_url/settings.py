bed8b9f2862484b860609d355d320f5c54501286
Yuriy <YuriyCherniyMail@yandex.ru>
2025-05-01 01:45:23 +0300

    actions

--- app/lazy_url/settings.py
+++ app/lazy_url/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool('DEBUG')
-DEBUG = True
+DEBUG = False
 
 # Security setting for production.
 if not DEBUG:
